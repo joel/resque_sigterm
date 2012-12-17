a0b827c16b5c28435dd68a8ed3e16b77beab44e7
Joel AZEMAR <joel.azemar@gmail.com>
2012-12-17 16:43:57 +0100

    Initial Commit

---
+++ Rakefile
@@ -0,0 +1,8 @@
+require 'rake'
+require 'resque/tasks'
+
+task 'resque:setup' do
+  require "#{File.dirname(__FILE__)}/myworker.rb"
+end
+
+
---
+++ worker.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('Gemfile', __FILE__)
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+require 'resque'
+require 'uri'
+
+class MyWorker
+  @queue = :worker
+
+  def self.perform
+    puts "Yeah i'm alive!!!"
+  end
+end
+
+begin
+  uri = URI.parse 'redis://localhost:6379'
+  _redis = Redis.new host: uri.host, port: uri.port
+  Resque.redis = Redis::Namespace.new :resque, redis: _redis
+  puts "Enqueue Job"
+  Resque.enqueue MyWorker, nil
+end
