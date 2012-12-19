b90465f08c467104d793dac0145cc2ba9f3f111e
Joel AZEMAR <joel.azemar@gmail.com>
2012-12-19 19:02:57 +0100

    Changes logs

--- monkey_patch_worker.rb
+++ monkey_patch_worker.rb
@@ -4,14 +4,14 @@ require 'resque/worker'
 module Resque
   class Worker
     def reserve
-      puts "Call on Subclass => worker.reserve"
+      puts "Called on monkey patch !"
       queues.each do |queue|
-        log! "Checking fucking #{queue}"
+        log! "Checking fucking queue #{queue}"
         if job = Resque.reserve(queue)
-          log! "Found fucking job on #{queue}"
+          log! "Found fucking job on queue #{queue}"
           return job
         else
-          log! "No found any fucking job on #{queue} i'm kill myself ! bye..."
+          log! "No found any fucking job on queue #{queue} so i'm kill myself ! bye..."
           sleep 1
           Process.kill 'TERM', self.pid
         end
